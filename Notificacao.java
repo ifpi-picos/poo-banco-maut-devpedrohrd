365a317dbc47e48ba5584baccf9d9c5ee8a36305
Pedro Henrique Damacena <126672685+devpedrohrd@users.noreply.github.com>
2023-10-19 05:57:01 -0300

    Delete Notificacao.java

--- Notificacao.java
+++
@@ -1,8 +0,0 @@
-public class Notificacao {
-    public void enviarEmail(String descricao, double valor) {
-        System.out.println("****** Notificacao ******");
-        System.out.println("Operacao realizada ->" + descricao);
-        System.out.println("Valor ->" + valor);
-        System.out.println("#### Notificacao concluida ####");
-    }
-}
