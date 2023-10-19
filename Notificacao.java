72097f7649bc2f9b9134e96cee5c27b061f39fec
Pedro Henrique Damacena <126672685+devpedrohrd@users.noreply.github.com>
2023-10-19 05:54:11 -0300

    Add files via upload

---
+++ Notificacao.java
@@ -0,0 +1,8 @@
+public class Notificacao {
+    public void enviarEmail(String descricao, double valor) {
+        System.out.println("****** Notificacao ******");
+        System.out.println("Operacao realizada ->" + descricao);
+        System.out.println("Valor ->" + valor);
+        System.out.println("#### Notificacao concluida ####");
+    }
+}
